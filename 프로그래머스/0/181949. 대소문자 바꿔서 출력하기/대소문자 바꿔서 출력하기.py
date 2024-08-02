5d5f58a8d6dd2ce27b1b28d6aa346f0d32a076b6
august <55475872+august-hw2@users.noreply.github.com>
2024-08-02 21:12:17 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 18.94 ms, Memory: 7.29 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,2 +1,10 @@
-str = input()
-print(str.swapcase())
+user = input()
+result = "" #문자열 선언
+
+if 1 <= len(user) <= 20 and user.isalpha():
+    for i in user:
+        if 'a' <= i <= 'z':  # 소문자일 경우
+            result += i.upper()
+        elif 'A' <= i <= 'Z':  # 대문자일 경우
+            result += i.lower()
+    print(result)
