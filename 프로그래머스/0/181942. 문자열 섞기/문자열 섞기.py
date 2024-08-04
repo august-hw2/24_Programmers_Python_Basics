d8c713c328e7a9c40b5ff0c8292e8cd9243d27ef
august <55475872+august-hw2@users.noreply.github.com>
2024-08-04 16:29:34 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -0,0 +1,12 @@
+def solution(str1, str2):
+
+    if 1<=len(str1)<=10 and 1<=len(str2)<=10 and len(str1)==len(str2):
+        if str1.islower() and str2.islower():
+            answer = ''
+            for i in range(0, len(str2)):
+                answer = answer+str1[i]+str2[i]
+        else:
+            return -1
+    else:
+        return -1
+    return answer
