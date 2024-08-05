0a341be9881c019fee89ea49d8d97671ea0faf45
august <55475872+august-hw2@users.noreply.github.com>
2024-08-05 22:28:29 +0900

    [level 0] Title: 코드 처리하기, Time: 25.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,18 +1,22 @@
 def solution(code):
     ret = ''
     mode = 0
+    
+    if 1<=len(code)<=100000:
 
-    for idx in range(len(code)):
+        for idx in range(len(code)):
 
-        if mode == 0: #모드 = 0
-            if code[idx] != '1' and idx%2 == 0:
-                ret += code[idx]
-            elif code[idx] == '1':
-                mode = 1
+            if mode == 0: #모드 = 0
+                if code[idx] != '1' and idx%2 == 0:
+                    ret += code[idx]
+                elif code[idx] == '1':
+                    mode = 1
 
-        else: #모드 = 1
-            if code[idx] != '1' and idx%2 != 0:
-                ret += code[idx]
-            elif code[idx] == '1':
-                mode = 0
-    return "EMPTY" if ret == "" else ret
+            else: #모드 = 1
+                if code[idx] != '1' and idx%2 != 0:
+                    ret += code[idx]
+                elif code[idx] == '1':
+                    mode = 0
+        return "EMPTY" if ret == "" else ret
+    
+    return -1
