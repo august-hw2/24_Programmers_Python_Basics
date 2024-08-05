210b23cea1dd3172853797cf6b7ecb4ab745e20d
august <55475872+august-hw2@users.noreply.github.com>
2024-08-05 20:39:54 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,6 +1,4 @@
 def solution(number, n, m):
     if 10<=number<=100 and 2<=n<=10 and 2<=m<=10:
-        if number%n == 0 and number%m == 0:
-            return 1
-        else: return 0
+        return 1 if number%n == 0 and number%m == 0 else 0
     else: return -1
