bf0d119427ee4de0d0826da602cb4582a2f022c9
august <55475872+august-hw2@users.noreply.github.com>
2024-08-05 20:59:03 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,14 +1,9 @@
 def solution(n):
     if 1<=n<=100:
         
-        result = 0
-        
-        if n%2 == 0:
-            for i in range(2, n+1, 2):
-                result += (i*i)
-            return result
+        if n%2:
+            return sum(range(1, n+1, 2))
         else:
-            for i in range(1, n+1, 2):
-                result += i
-            return result
+            return sum(i**2 for i in range(2, n+1, 2))
+        
     else: return -1
