0a76de1a5a7723675637af50d71de5e81de41a01
august <55475872+august-hw2@users.noreply.github.com>
2024-08-05 21:48:58 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.py
@@ -1,7 +1,7 @@
 def solution(a, b, flag):
     if -1000<=a<=1000 and -1000<=b<=1000:
-        if flag == True:
+        if flag:
             return a+b 
-        elif flag == False:
+        else:
             return a-b 
     else: return -1
