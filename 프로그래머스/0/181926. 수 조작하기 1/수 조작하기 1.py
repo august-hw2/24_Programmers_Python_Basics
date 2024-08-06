07222430604ccc121f46f267cb38daec62aea178
august <55475872+august-hw2@users.noreply.github.com>
2024-08-06 21:46:57 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.86 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,17 +1,11 @@
 def solution(n, control):
 
     if -100000<=n<100000 and 1<=len(control)<=100000 and control.islower():
-        for i in range(len(control)):
-            if "w" == control[i]:
-                n += 1
-            elif "s" == control[i]:
-                n -= 1
-            elif "d" == control[i]:
-                n += 10
-            elif "a" == control[i]:
-                n -= 10
-            else:
-                return -1
+        
+        control_dict = {"w":1, "s":-1, "d":10, "a":-10}
+        
+        for i in control:
+            n += control_dict[i]
         return n
 
     else: 
