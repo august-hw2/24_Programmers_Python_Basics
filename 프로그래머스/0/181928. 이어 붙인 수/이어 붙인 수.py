b01f5ea33e9a7ed1fbbe040b30e55431e14b0032
august <55475872+august-hw2@users.noreply.github.com>
2024-08-06 20:44:48 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -2,12 +2,12 @@ def solution(num_list):
     if 2<=len(num_list)<=10:
         even = "" #짝수
         odd = "" #홀수
-        for i in range(len(num_list)):
-            if 1<=num_list[i]<=9:
-                if num_list[i]%2: #홀수인 경우
-                    odd += str(num_list[i])
+        for i in num_list:
+            if 1<=i<=9:
+                if i%2: #홀수인 경우
+                    odd += str(i)
                 else: #짝수인 경우
-                    even += str(num_list[i])
+                    even += str(i)
             else:
                 return -1
         return int(even)+int(odd)
