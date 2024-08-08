5de00f8d2bbbc1e1c1b5f82526b5eaca956b56c9
august <55475872+august-hw2@users.noreply.github.com>
2024-08-08 16:10:20 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,40 @@
+def solution(a, b, c, d):
+    if 1<=a<=6 and 1<=b<=6 and 1<=c<=6 and 1<=d<=6:
+        arr = [a, b, c, d]
+        res_arr = [] #처음 나온 값만 저장
+        temp_arr = [] #중복인 값만 저장
+        
+        
+        for i in arr:
+            if i not in res_arr: #해당 값이 없으면 배열에 저장
+                res_arr.append(i)
+            else: #해당 값이 있으면 중복이므로, 다른 배열에 저장
+                temp_arr.append(i)
+        
+        if len(temp_arr) == 3: #모두 같은 값 -> 1개 제외한 나머지 모두 저장
+            return 1111*a
+        elif len(temp_arr) == 2: #4개 중 2개씩 중복 혹은 3개 중복 -> 중복된 값 2개 저장
+            if len(set(temp_arr)) == 1: #temp에서도 중복이 발생한 경우
+                for i in res_arr:
+                    if i == temp_arr[0]:
+                        p = i
+                        res_arr.remove(i)
+                return (10*p+res_arr[0])**2
+                #pass
+                #p = temp_arr[0]
+                #q = set(temp_arr)&set(res_arr)
+                #return (10*p+q[0])**2
+            elif len(set(temp_arr)) == 2: #temp에서 중복이 발생하지 않은 경우
+                return (res_arr[0]+res_arr[1])*abs(res_arr[0]-res_arr[1])                
+        elif len(temp_arr) == 1: #4개 중 2개만 중복 -> 중복된 값 1개만 저장       
+            res = 1
+            
+            for i in res_arr:
+                if i != temp_arr[0]:
+                    res *= i
+            return res
+        
+        elif len(temp_arr) == 0: #서로 다른 값 -> 저장되지 않음
+            return min(arr)
+            
+    return -1
