895b8a0b6a5879a8172f9d3b7748dbb921114f10
august <55475872+august-hw2@users.noreply.github.com>
2024-08-08 20:33:42 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -6,21 +6,9 @@ def solution(my_string, queries):
         re_list = [] #빈 리스트
 
         for s, e in queries: #queries의 요소 값
-            re_arr = reversed(arr[s:e+1]) #리스트 슬라이싱 -> 끝 숫자 포함하지 않으므로, +1
-            re_list = list(re_arr)
+            arr[s:e+1] = arr[s:e+1][::-1]
 
-            i = 0 
-
-            while s < e+1:
-
-                arr[s] = re_list[i]
-                s += 1
-                i += 1
-
-
-            re_arr = [] #초기화
-
-    return ''.join(arr)
+        return ''.join(arr)
     
     return -1
         
