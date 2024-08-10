2cfb91b50745c557dc45fc28bfb169e098c112cd
august <55475872+august-hw2@users.noreply.github.com>
2024-08-10 14:56:51 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -3,11 +3,11 @@ def solution(my_string):
         
         res = [0] * 52
         
-        for i in range(len(my_string)):
-            if 65 <= ord(my_string[i]) <= 90: #대문자
-                res[ord(my_string[i]) - 65] += 1
-            elif 97 <= ord(my_string[i]) <= 122: #소문자
-                res[ord(my_string[i]) - 71] += 1
+        for i in my_string:
+            if i.isupper():#대문자
+                res[ord(i) - 65] += 1
+            else:  #소문자
+                res[ord(i) - 71] += 1
             
         return res
         
