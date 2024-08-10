cb203529f82678b5893bcce34ad08804f71b01ca
august <55475872+august-hw2@users.noreply.github.com>
2024-08-10 14:18:42 +0900

    [level 0] Title: 세로 읽기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,7 +1,7 @@
 def solution(my_string, m, c):
     if 1<=m<=len(my_string) and len(my_string)<=1000 and my_string.islower():
         
-        return my_string[c-1::m]
+        return my_string[c-1::m] #입력받은 문자열의 c-1의 자리에 있는 글자부터 끝까지 m씩 증가하여 이은 문자열 리턴
     
     else:
         return -1
