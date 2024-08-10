4dc23d894479b675618ba8372668302b7664c100
august <55475872+august-hw2@users.noreply.github.com>
2024-08-10 14:40:38 +0900

    [level 0] Title: qr code, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,13 +1,7 @@
 def solution(q, r, code):
     if 0<=r<q and q<=20 and r<len(code)<=1000 and code.islower():
         
-        res = ""
-        
-        for i in range(len(code)):
-            if i%q == r:
-                res = res + code[i]
-                
-        return res
+        return code[r::q] #r부터 q씩 증가하는 문자열 반환
         
     else:
         return -1
