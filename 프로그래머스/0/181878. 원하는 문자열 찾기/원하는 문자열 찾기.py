354f8b4f13a50d1e172bf8b2383a064c234b7649
august <55475872+august-hw2@users.noreply.github.com>
2024-08-13 21:09:56 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,13 +1,7 @@
 def solution(myString, pat):
     if 1<=len(myString)<=100000 and 1<=len(pat)<=300:
         
-        if len(pat)>len(myString):
-            return 0
-        
-        if pat.lower() in myString.lower():
-            return 1
-        else:
-            return 0
+        return int(pat.lower() in myString.lower())
         
     else:
         return -1
