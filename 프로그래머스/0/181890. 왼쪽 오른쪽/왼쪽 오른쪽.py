2c1f8e40c3866f2f0399028fc282977bba604093
august <55475872+august-hw2@users.noreply.github.com>
2024-08-13 16:12:26 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -2,18 +2,15 @@ def solution(str_list):
     
     if 1<=len(str_list)<=20:
         
-        res = [] #결과값 리스트 선언
         
         for i, j in enumerate(str_list):
             if j == "l": #왼쪽인 경우
-                res+=str_list[0:i]
-                return res
+                return str_list[0:i]
                 
             elif j == "r": #오른쪽인 경우
-                res+=str_list[i+1:]
-                return res
-            
-        return res #"l" 또는 "r"이 없는 경우
+                return str_list[i+1:]
+
+        return [] #"l" 또는 "r"이 없는 경우
     
     else:
         return -1
